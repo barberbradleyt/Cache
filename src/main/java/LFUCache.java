682d5f3f52cfb390e23f3491645deee72ed31422
bradleyb <bradleyb@sfu.ca>
2023-12-07 10:15:08 -0800

    Added LFU eviction logic and base LFU test

--- src/main/java/LFUCache.java
+++ src/main/java/LFUCache.java
@@ -1,7 +1,6 @@
-import java.lang.ref.SoftReference;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
@@ -20,7 +19,12 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class LFUCache<K, V> implements Cache<K, V> {
     private int maxSize;
     private long expiryTimeInMillis;
-    private Map<K, SoftReference<V>> keyValueMap;
+    //mapping of key to associated value
+    private Map<K, LFUCacheEntry<K,V>> keyValueMap;
+    //mapping of key to value use (lookup) frequency
+    private Map<K, Integer> keyFrequencyMap;
+    //map of use frequency to list of value entries
+    private ConcurrentSkipListMap<Integer, DoublyLinkedLFUCacheEntryList<K, V>> frequencyEntryMap = new ConcurrentSkipListMap<>();
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     /**
@@ -31,9 +35,14 @@ public class LFUCache<K, V> implements Cache<K, V> {
      * @param maxCacheSize          the maximum number of entries to be maintained in the cache
      */
     LFUCache(long expiryTimeInSeconds, int maxCacheSize) {
+        if (expiryTimeInSeconds <= 0 || maxCacheSize <= 0) {
+            throw new IllegalArgumentException("expiryTimeInSeconds and maxCacheSize must both be positive, non-zero values");
+        }
+
         expiryTimeInMillis = expiryTimeInSeconds * 1000;
         maxSize = maxCacheSize;
         keyValueMap = new ConcurrentHashMap<>(maxCacheSize);
+        keyFrequencyMap = new ConcurrentHashMap<>(maxCacheSize);
     }
 
     /**
@@ -47,7 +56,21 @@ public class LFUCache<K, V> implements Cache<K, V> {
     public V get(K key) {
         lock.readLock().lock();
         try {
-            return Optional.ofNullable(keyValueMap.get(key)).map(SoftReference::get).orElse(null);
+            LFUCacheEntry<K,V> entry = keyValueMap.get(key);
+            if (entry == null) return null;
+
+            //update frequency of entry associated with key
+            LFUCacheEntry<K,V> updatedEntry = new LFUCacheEntry<>(key, entry.value());
+            int prevFrequency = keyFrequencyMap.get(key);
+
+            removeEntryFromFrequencyList(prevFrequency, entry);
+            keyValueMap.remove(key);
+            keyFrequencyMap.remove(key);
+            keyValueMap.put(key, updatedEntry);
+            keyFrequencyMap.put(key, prevFrequency + 1);
+            frequencyEntryMap.computeIfAbsent(prevFrequency + 1, entryList -> new DoublyLinkedLFUCacheEntryList<>()).add(updatedEntry);
+
+            return updatedEntry.value();
         } finally {
             lock.readLock().unlock();
         }
@@ -74,12 +97,34 @@ public class LFUCache<K, V> implements Cache<K, V> {
 
         lock.writeLock().lock();
         try {
-            if (keyValueMap.size() == maxSize) {
-                removeLFUEntry();
-            }
+            if (!keyValueMap.containsKey(key)) { //adding new entry
+                LFUCacheEntry<K, V> entryToAdd = new LFUCacheEntry<>(key, value);
 
-            SoftReference<V> valueReference = new SoftReference<>(value);
-            keyValueMap.put(key, valueReference);
+                if (keyValueMap.size() == maxSize) { //need to remove LFU entry
+                    int leastFrequency = frequencyEntryMap.firstKey();
+                    LFUCacheEntry<K,V> entryToRemove = frequencyEntryMap.get(leastFrequency).head();
+
+                    removeEntryFromFrequencyList(leastFrequency, entryToRemove);
+                    keyValueMap.remove(entryToRemove.key());
+                    keyFrequencyMap.remove(entryToRemove.key());
+                }
+
+                frequencyEntryMap.computeIfAbsent(1, entryList -> new DoublyLinkedLFUCacheEntryList<>()).add(entryToAdd);
+                keyValueMap.put(key, entryToAdd);
+                keyFrequencyMap.put(key, 1);
+            }
+            else { //updating existing entry
+                LFUCacheEntry<K,V> entryToUpdate = keyValueMap.get(key);
+                LFUCacheEntry<K,V> updatedEntry = new LFUCacheEntry<>(key, value);
+                int prevFrequency = keyFrequencyMap.get(key);
+
+                removeEntryFromFrequencyList(prevFrequency, entryToUpdate);
+                keyValueMap.remove(key);
+                keyFrequencyMap.remove(key);
+                keyValueMap.put(key, updatedEntry);
+                keyFrequencyMap.put(key, prevFrequency + 1);
+                frequencyEntryMap.computeIfAbsent(prevFrequency + 1, entryList -> new DoublyLinkedLFUCacheEntryList<>()).add(updatedEntry);
+            }
         } finally {
             lock.writeLock().unlock();
         }
@@ -101,9 +146,80 @@ public class LFUCache<K, V> implements Cache<K, V> {
         return keyValueMap.size();
     }
 
-    private void removeLFUEntry() {
-        //TODO: compute LFU entry and remove
+    private void removeEntryFromFrequencyList(int frequency, LFUCacheEntry<K,V> entry) {
+        frequencyEntryMap.get(frequency).remove(entry);
+        if (frequencyEntryMap.get(frequency).size() == 0) {
+            frequencyEntryMap.remove(frequency);
+        }
+    }
+}
+
+class LFUCacheEntry<K, V> {
+    private K key;
+    private V value;
+    LFUCacheEntry<K,V> next;
+    LFUCacheEntry<K,V> prev;
+
+    public LFUCacheEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
     }
 
-    //TODO: add expiry mechanism
+    public K key() {
+        return key;
+    }
+
+    public V value() {
+        return value;
+    }
+}
+
+class DoublyLinkedLFUCacheEntryList<K, V> {
+    private int size;
+    private LFUCacheEntry<K,V> head;
+    private LFUCacheEntry<K,V> tail;
+
+    public LFUCacheEntry<K, V> head() {
+        return head;
+    }
+
+    public LFUCacheEntry<K, V> tail() {
+        return tail;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void add(LFUCacheEntry<K,V> newEntry) {
+        if (head == null) {
+            head = newEntry;
+        } else {
+            tail.next = newEntry;
+            newEntry.prev = tail;
+        }
+
+        tail = newEntry;
+        size++;
+    }
+
+    public void remove(LFUCacheEntry<K,V> entry) {
+        //left side of entry
+        if (entry.prev == null) {
+            head = entry.next;
+        }
+        else {
+            entry.prev.next = entry.next;
+        }
+
+        //right side of entry
+        if (entry.next == null) {
+            tail = entry.prev;
+        }
+        else {
+            entry.next.prev = entry.prev;
+        }
+
+        size--;
+    }
 }
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,37 +1,134 @@
+import java.util.Objects;
+
 public class Solution {
     public static void main(String[] args) {
         getAbsentKey();
         basicPutGetTest();
+        LFUTest();
     }
 
     private static void getAbsentKey() {
+        System.out.println("Running getAbsentKey() test...");
+
         LFUCache<String, String> cache = new LFUCache<>(10, 10);
         LFUCache<String, Integer> cache2 = new LFUCache<>(10, 10);
 
-        //Test retrieving an absent key - String value
-        assert cache.size() == 0;
-        assert cache.get("a").isEmpty();
+        try {
+            //Test retrieving an absent key - String value
+            assertEquals(cache.size(), 0);
+            assertEquals(cache.get("a"), null);
 
-        //Test retrieving an absent key - Integer value
-        assert cache.size() == 0;
-        assert cache2.get("a") == null;
+            //Test retrieving an absent key - Integer value
+            assertEquals(cache2.size(), 0);
+            assertEquals(cache2.get("a"), null);
+        } catch (IllegalStateException e) {
+            System.out.printf("Test FAILED: %s%n", e.getMessage());
+            return;
+        }
+        System.out.println("Test SUCCEEDED");
     }
 
     private static void basicPutGetTest() {
+        System.out.println("Running basicPutGetTest() test...");
+
         LFUCache<String, String> cache = new LFUCache<>(10, 10);
+        LFUCache<String, Integer> cache2 = new LFUCache<>(10, 10);
 
-        //Test adding and retrieving entries
-        cache.put("a", "A");
-        cache.put("b", "B");
-        cache.put("c", "C");
-        cache.put("d", "D");
-        cache.put("e", "E");
+        try {
+            //Test adding and retrieving entries - String value
+            cache.put("a", "A");
+            cache.put("b", "B");
+            cache.put("c", "C");
+            cache.put("d", "D");
+            cache.put("e", "E");
 
-        assert cache.size() == 5;
-        assert cache.get("a").equals("A");
-        assert cache.get("b").equals("B");
-        assert cache.get("c").equals("C");
-        assert cache.get("d").equals("D");
-        assert cache.get("e").equals("E");
+            assertEquals(cache.size(), 5);
+            assertEquals(cache.get("a"), "A");
+            assertEquals(cache.get("b"), "B");
+            assertEquals(cache.get("c"), "C");
+            assertEquals(cache.get("d"), "D");
+            assertEquals(cache.get("e"), "E");
+
+            //Test adding and retrieving entries - Integer value
+            cache2.put("a", 1);
+            cache2.put("b", 2);
+            cache2.put("c", 3);
+            cache2.put("d", 4);
+            cache2.put("e", 5);
+
+            assertEquals(cache2.size(), 5);
+            assertEquals(cache2.get("a"), 1);
+            assertEquals(cache2.get("b"), 2);
+            assertEquals(cache2.get("c"), 3);
+            assertEquals(cache2.get("d"), 4);
+            assertEquals(cache2.get("e"), 5);
+        } catch (IllegalStateException e) {
+            System.out.printf("Test FAILED: %s%n", e.getMessage());
+            return;
+        }
+
+        System.out.println("Test SUCCEEDED");
+    }
+
+    private static void LFUTest() {
+        System.out.println("Running LFUTest() test...");
+
+        LFUCache<String, String> cache = new LFUCache<>(10, 5);
+        LFUCache<String, Integer> cache2 = new LFUCache<>(10, 5);
+
+        try {
+            //Test adding and retrieving entries beyond max size - String value
+            cache.put("a", "A");
+            cache.put("b", "B");
+            cache.put("c", "C");
+            cache.put("d", "D");
+            cache.put("e", "E");
+
+            cache.get("b");
+            cache.get("c");
+            cache.get("d");
+
+            cache.put("f", "F"); //a->A is oldest LFU, should be removed
+            assertEquals(cache.size(), 5);
+            assertEquals(cache.get("a"), null);
+            assertEquals(cache.get("f"), "F");
+
+            cache.put("g", "G"); //e->E is oldest LFU, should be removed
+            assertEquals(cache.size(), 5);
+            assertEquals(cache.get("e"), null);
+            assertEquals(cache.get("g"), "G");
+
+            //Test adding and retrieving entries beyond max size - Integer value
+            cache2.put("a", 1);
+            cache2.put("b", 2);
+            cache2.put("c", 3);
+            cache2.put("d", 4);
+            cache2.put("e", 5);
+
+            cache2.get("b");
+            cache2.get("c");
+            cache2.get("d");
+
+            cache2.put("f", 6); //a->A is oldest LFU, should be removed
+            assertEquals(cache2.size(), 5);
+            assertEquals(cache2.get("a"), null);
+            assertEquals(cache2.get("f"), 6);
+
+            cache2.put("g", 7); //e->E is oldest LFU, should be removed
+            assertEquals(cache2.size(), 5);
+            assertEquals(cache2.get("e"), null);
+            assertEquals(cache2.get("g"), 7);
+        } catch (IllegalStateException e) {
+            System.out.printf("Test FAILED: %s%n", e.getMessage());
+            return;
+        }
+
+        System.out.println("Test SUCCEEDED");
+    }
+
+    private static void assertEquals(Object expected, Object actual) {
+        if (!Objects.equals(expected, actual)) {
+            throw new IllegalStateException(String.format("expected='%s'; actual='%s'", expected, actual));
+        }
     }
 }
